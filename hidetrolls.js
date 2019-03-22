7b6fec191e7e0a778a5dbe7cf19702de50d59545
root <info@serg-smirnoff.com>
2019-03-22 22:12:31 +0300

    Fix user-info block‏

--- hidetrolls.js
+++ hidetrolls.js
@@ -1,14 +1,14 @@
 ﻿// ==UserScript==
 // @name Hide Trolls
 // @namespace Violentmonkey Scripts
-// @description Hide trolls via nickname
+// @description Скрываем тролей на форуме по никнейму
 // @grant none
 // @author Sergey Smirnov
 // @license MIT
 // @version 1.0
-// @include https://www.newkaliningrad.ru/forum/*]https://www.newkaliningrad.ru/forum/*
-// @match https://www.newkaliningrad.ru/forum/*]https://www.newkaliningrad.ru/forum/*
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @include https://www.newkaliningrad.ru/forum/*
+// @match https://www.newkaliningrad.ru/forum/*
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // ==/UserScript==
 (function (window, undefined) {
     var w;
@@ -22,13 +22,15 @@
     }
     jQuery(document).ready(function() {
         jQuery(".topic .post_block").each(function() {
-            var nickname = jQuery(this).find(".post_wrap .author a span");
+            var nickname = jQuery(this).find(".post_wrap .row2 .author a span");
             //nicknames to hide
-            if ( (nickname.text() == 'Virgo-ша') || (nickname.text() == 'zt39') || (nickname.text() == 'Rabel')){
-              jQuery(this).css('display', 'none');
+            if ( (nickname.text() == 'Virgo-ша') || (nickname.text() == 'zt39') || (nickname.text() == 'Rabel') ){
+              console.log(jQuery(this));
+              jQuery(this).find(".post_wrap").css('display', 'none');
             }
-        });   
+        }); 
+        jQuery(".user-info").css('display','block');
     });
-    if (/http:\/\/userscripts.org/.test(w.location.href)) {
-    }
+    /*if (/http:\/\/userscripts.org/.test(w.location.href)) {
+    }*/
 })(window);
